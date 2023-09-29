eaa79fcf3bae2c52a1614377b27f1c9f674b7420
insirenator <arsshakeeb149@gmail.com>
2023-09-29 10:32:17 +0530

    Added: Migration for the Main DB and test DB

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,11 +1,17 @@
 import { Pool } from "pg";
+import { config } from "dotenv";
+config();
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  database: "FlipMart",
-  password: "POSTgres@149",
-  port: 5432,
+  connectionString: process.env.DB_URL,
 });
 
 export default pool;
+
+// {
+//   host: "localhost",
+//   user: "postgres",
+//   database: "FlipMart",
+//   password: "POSTgres@149",
+//   port: 5432,
+// }
